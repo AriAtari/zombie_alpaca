cf4d6c442e7ed7cc45d4be33d127011c5de8fe44
EliasTaira <elias_taira@hotmail.com>
2022-04-14 16:51:47 -0400

    Finished scent simulation

--- zombie_model.py
+++ zombie_model.py
@@ -67,7 +67,8 @@ class zombie:
         
         # add any special attributes here
         
-        self.name = None # only here so code doesn't return an error
+        self.smell_human = False
+        self.human_loc = [0,0]
         
     def place_at(self, coord):
         """Place entity at coordinates (x,y).
@@ -82,9 +83,17 @@ class zombie:
 
     def move(self):
         """Move the zombie by randomly pushing it in both directions."""
-        self.x += np.random.randint(low=-1, high=2)
-        self.y += np.random.randint(low=-1, high=2)
+        
+        if self.smell_human == True:
 
+            self.x += (self.human_loc[0]-self.x)/abs(self.human_loc[0]-self.x)*np.random.randint(low=0, high=2)
+
+            self.y += (self.human_loc[0]-self.x)/abs(self.human_loc[0]-self.x)*np.random.randint(low=0, high=2)
+
+        else:
+            self.x += np.random.randint(low=-1, high=2)
+            self.y += np.random.randint(low=-1, high=2)
+            
 class alpacalypse:
     """alpacalipse class. There is nothing otherwise."""
     _all_entities = None
@@ -167,6 +176,12 @@ class alpacalypse:
                         self.humans.pop(ip1)
                         self.zombies.append(zombie())
                         self.zombies[-1].place_at(coord=(p1.x, p1.y))
+                        
+                if np.sqrt((p1.x-p2.x)**2 + (p1.y-p2.y)**2) <= p2.smell_radius:
+                    p2.smell_human = True
+                    p2.human_loc[0] = p1.x
+                    p2.human_loc[1] = p1.y
+                    
                     continue
 
         # update the list
